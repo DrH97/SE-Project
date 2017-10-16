388945bcc12fdd64e00785a0a7837b9243ba8bab
Brigidmunyiri <32155761+Brigidmunyiri@users.noreply.github.com>
2017-10-16 09:50:37 -0700

    Add files via upload
    homepage php

--- homepage.php
+++ homepage.php
@@ -1,7 +1,60 @@
-<?php
-@include 'layout/navbar.php';
-
- ?>
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>LIBRARY SYSTEM</title>
+	<link rel="stylesheet" type="text/css" href="navigation.css">
+</head>
+<body style="background-color: green">
+<div class="topnav">
+            <div class="dropdown0">
+<a>&#9776;</a>
+<a href="homepage.html"><strong> LIBRARY SYSTEM </strong></a>
+            <div class="links">
+                <ul>
+                    <li><a class="active"> Home </a></li>
+                    <li><a href="Librarian.html"> Librarian </a></li>
+                    <li><a href="Books.html"> Books </a></li>
+                    <li><a href="Student.html"> Student </a></li>
+                    <li><a href="fines.html"> Fines </a></li>
+                </ul>
+            </div>
+            </div>
+            <div class="account">  
+                <div class="dropdown3">
+                    <a>&#128269;</a>
+                    <div class="dropdown-search">
+                        <form method="get" action="/search" id="search">
+                            <input type="text" class="details" placeholder="Search..."/>
+                        </form>
+                    </div>
+                </div>
+                <div class="dropdown1">
+                    <a>Sign up</a>
+                    <div class="dropdown-signup">
+                        <form>
+                            <input type="email" class="details" placeholder="Your email address" required><br>
+                            <br><input type="text" class="details" placeholder="Username" required><br>
+                            <br><input type="password" class="details" placeholder="Password" required><br>
+                            <br><input type="password" class="details" placeholder="Confirm password" required><br>
+                            <br><input type="submit" class="button" value="Submit"><br>
+                            <br>Register / Create account.
+                        </form>
+                    </div>
+                </div>
+                <div class="dropdown2">
+                    <a>Login</a>
+                    <div class="dropdown-login">
+                        <form>
+                            <input type="email" class="details" placeholder="Username / Email" required><br>
+                            <br><input type="password" class="details" placeholder="Password" required><br>
+                            <br><input type="checkbox"> Remember me.<br>
+                            <br><input type="submit" class="button" value="Login"><br>
+                            <br><a href="">Forgot password?</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div id="image">
             <div class="display">
                 <img src="library system.jpg" class="imgHome" alt=""/>
@@ -11,7 +64,6 @@
             </div>
         </div>
         <div class="tabs">
-
             <a href=""><h2>Join Us</h2></a>
             <p>Welcome to our library where every book you need we've got it.</p>
             <p>Sign up or login to our site by filling in the forms at the top-right corner.</p>
@@ -21,8 +73,17 @@
             <p>For more information, please click or visit some of the links provided on this website.</p>
             <p>Connect with us on Facebook, Twitter, Google+ etc. and share your feedback</p>
         </div>
+        <footer>
+            <p>&#169;2017 Copyright by Us Inc. All Rights Reserved.</p>
+            <p>
+                <a href="">Google+</a>
+                <a href="">Facebook</a>
+                <a href="">Twitter</a>
+                <a href="">Terms of use</a>
+                <a href="">Privacy Policy</a>
+            </p>
+        </footer>
 
-        </div>
-<?php
-  @include 'layout/footer.php';
- ?>
+
+</body>
+</html>
