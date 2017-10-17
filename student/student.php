254bcf6a9f7cc613a7fc4ddade7776af9d9e2bb7
KenCalvins <32137632+KenCalvins@users.noreply.github.com>
2017-10-17 11:20:21 +0300

    Add files via upload

---
+++ student/student.php
@@ -0,0 +1,47 @@
+<html>
+<header>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<style type="text/css">
+	.jumbotron {
+		background-color: #00CC00;
+
+	}
+</style>
+</header>
+<style>
+<boy78dy>
+</style>
+
+<div class="jumbotron">
+	<h1> STUDENT</h1>
+</div>
+
+	
+
+<form name="student" action="form.php" method="POST">
+
+
+<fieldset>
+
+<legend>STUDENT'S REGISTER FORM
+</legend>
+<iframe height="200" width="250"></iframe><br><br>
+
+<label>Name:<br><input type="text" name="name" value="" placeholder="Enter name" size="35" required></label><br><br>
+<label>Student No.:<br><input type="text" name="student_no" value="" placeholder="Enter student number" size="35" required></label><br><br>
+<label>Course:<br><input type="text" name="course" value="" placeholder="Enter course" size="35" required></label><br><br>
+<label>Date:<br><input type="date" name="Collection_date" value="" required></label><br><br>
+<input type="submit">
+<input type="reset">
+
+
+</fieldset>
+</form>
+</body>
+</html>
